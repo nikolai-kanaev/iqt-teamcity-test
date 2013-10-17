e64a01ac8478a4b5981cc148c8b147f61c339d05
unknown <nikkan@SELNIKKAN.sweden.ecsoft>
2013-10-17 12:54:44 +0200

    auto commit

--- Rakefile.rb
+++ Rakefile.rb
@@ -10,3 +10,16 @@ msbuild :msbuild do |msb|
   msb.targets = [ :Clean, :Build ]      
   msb.solution = "src/iqt-teamcity-test.sln"
 end
+
+task :gittask do
+  cmd = 'git add -A'
+  system(cmd)
+  cmd1 = 'git commit -m "auto commit"'
+  system(cmd1)
+  cmd2 = 'git push origin master'
+  system(cmd2)
+end
+
+task :release => [:msbuild, :gittask] do
+  puts 'What happens in TeamCity stays in TeamCity.'
+end

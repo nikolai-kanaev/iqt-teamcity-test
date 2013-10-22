db11a062d5ab2a14ab7f96c5e03b505ed1a02e95
unknown <nikkan@SELNIKKAN.sweden.ecsoft>
2013-10-22 09:45:01 +0200

    Released version 1.2.0.0

--- src/iqt-teamcity-test/Properties/AssemblyInfo.cs
+++ src/iqt-teamcity-test/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Interfleet")]
 [assembly: AssemblyProduct("TeamCity Test")]
 [assembly: AssemblyCopyright("Nikolai Kanaev")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
